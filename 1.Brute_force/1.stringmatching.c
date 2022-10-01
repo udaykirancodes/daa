f976f537b9594b3567f80e904f55c0ce54c287ef
UDAY KIRAN <90251265+udaykirancodes@users.noreply.github.com>
2022-10-01 10:31:45 +0530

    Update 1.stringmatching.c

--- 1.Brute_force/1.stringmatching.c
+++ 1.Brute_force/1.stringmatching.c
@@ -1,53 +1,29 @@
 #include<stdio.h>
 #include<string.h>
-
-int main()
-{
-    int m,n,j,f = -1;
-
-    printf("\nEnter the size of main string : ");
-    scanf("%d",&m);
-
-    char main [m];
-    printf("\nEnter the  main string : ");
-    gets(main);
-    printf("The main string is : %s \n",main);
-    
-    printf("\nEnter the size of search string : ");
-    scanf("%d",&n);
-
-    char search [n];
-    printf("\nEnter the  search string : ");
-    gets(search);
-    printf("The search string is :%s\n",search);
-
-    for (int i = 0; i < m-n; i++)
-    {
-        for (  j = i; j < i+n; j++)
-        {
-            if (main[j]!=search[j-i])
-            {
-                break;
-            }
-            
-        }
-            if (j == i+n)
-            {
-                f = i;
-                break;
-            }
-        
-    }
-
-    if ( f== -1)
-    {
-        printf("\nString not found\n");
-    }
-    else
-    {
-        printf("\nString  starts at %d & ends at %d  indices \n",f+1,f+n);
-    }
-    
-    
-    return 0;
+int main(){
+	char mai[60],sub[20];
+	int flag=-1;
+	printf("enter the main string:");
+	gets(mai);
+	printf("enter the sub string to search:");
+	gets(sub);
+//	scanf("%[^\n]")
+	int l1,l2,i,j;
+	l1=strlen(mai);
+	l2=strlen(sub);
+	for(i=0;i<l1-l2;i++){
+		for(j=i;j<i+l2;j++){
+			if(mai[j]!=sub[j-i]){
+				break;
+			}
+		}
+		if(j==i+l2){
+			flag=i;
+		}
+	}
+	if(flag!=-1){
+		printf("String \'%s\' found in \'%s\' at index %d\n",sub,mai,flag);
+	}else{
+		printf("String \'%s\' not  found in \'%s\' \n",sub,mai);
+	}
 }
